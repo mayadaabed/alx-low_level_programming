706fdc91f77ddf13bcb2e6aa82f2432b071253ab
Mayada Almobayed <abedmayada4@gmail.com>
2023-03-17 15:11:55 -0700

    My Proccessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entery Point
+ *
+ * Description: C Programm for print 
+ *
+ * return (0)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return(0);
+}
